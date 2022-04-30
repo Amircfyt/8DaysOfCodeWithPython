b62ff674c708fa1d8544ea8cbf22d8550d7e9ba3
Amir cheraghi <100447221+Amircfyt@users.noreply.github.com>
2022-04-30 22:22:46 +0430

    Create 7Day.py

---
+++ 7Day.py
@@ -0,0 +1,13 @@
+import colorgram
+
+colors = colorgram.extract('Image.jpg', 30)
+rgb_colors = []
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+print(rgb_colors)
+
